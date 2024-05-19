a2e5383234810db2c305853d8deba6ccdaf38e59
Liam Cohen <licohen@paul-f4Br8s2.clusters.42paris.fr>
2024-05-19 14:47:00 +0200

    majfichiernv

--- srcs/ft_isdigit.c
+++ srcs/ft_isdigit.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: licohen <licohen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/18 21:59:06 by user              #+#    #+#             */
-/*   Updated: 2024/05/18 22:05:47 by user             ###   ########.fr       */
+/*   Created: 2024/05/19 14:37:02 by licohen           #+#    #+#             */
+/*   Updated: 2024/05/19 14:37:05 by licohen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isdigit(int c)
+int	ft_isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
-    {
-        return (1);
-    }
-    return (0);
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
 }
