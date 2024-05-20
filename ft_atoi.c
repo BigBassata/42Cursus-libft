6794f5ab89e503320161e9fdb28d9fb03306c36d
Liam Cohen <liamcohen.pro@gmail.com>
2024-05-20 17:34:28 +0200

    deletedirectorysrc

---
+++ ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: licohen <licohen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 18:19:58 by licohen           #+#    #+#             */
+/*   Updated: 2024/05/20 16:23:14 by licohen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int i;
+	int num;
+	int sign;
+
+	i = 0;
+	num = 0;
+	sign = 1;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		num = (nptr[i] - '0') + 10 * num;
+		i++;
+	}
+	return (num * sign);
+}
---
+++ ft_memchr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: licohen <licohen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/19 17:10:33 by licohen           #+#    #+#             */
+/*   Updated: 2024/05/20 16:55:01 by licohen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	c = (unsigned char) c;
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s)[i] == c)
+			return ((void *)s + i);
+		i++;
+	}
+	return (NULL);
+}
+
+int main()
+{
+    const char str1[] = "Hello, World!";
+    char *result1 = ft_memchr(str1, 'W', ft_strlen(str1));
+    if (result1 != NULL) {
+        printf("Caractère 'W' trouvé à l'adresse %p: %c\n", result1, *result1);
+    } else {
+        printf("Caractère 'W' non trouvé\n");
+    }
+    return 0;
+}
